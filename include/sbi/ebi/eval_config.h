a65ecfd0d4d740667ac6f49442d4053f44bc0358
GartonChan <504965844@qq.com>
2024-03-06 14:49:51 +0800

    Add ebi extension module

---
+++ include/sbi/ebi/eval_config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <util/eval_config.h>
+
+extern volatile int max_pmp;
+extern volatile int pmp_enable;
+extern volatile int id_split;
+extern volatile int tlb_cache;
+extern volatile int fragmented;
---
+++ lib/sbi/ebi/eval/eval_config.c
@@ -0,0 +1,7 @@
+#include <sbi/ebi/eval_config.h>
+
+volatile int max_pmp = 8;
+volatile int pmp_enable = 1;
+volatile int id_split = 1;
+volatile int tlb_cache = 1;
+volatile int fragmented = 0;
